9c5a8b1f9f2a4585b0017de37af001afd451ef91
Attila Repka <repka.attila@yandex.com>
2025-04-01 21:59:42 +0200

    feat(ft): add background/spacing/output settings

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,53 @@
 mod characters;
 
 use arboard::Clipboard;
-use clap::Parser;
+use clap::{ArgAction, Parser};
 use std::io;
 
 #[derive(Parser, Debug)]
 #[command(author, version, about = None, long_about = None)]
 struct Args {
-    /// Space width of output
-    #[arg(long)]
+    /// Input text to emojify
+    #[arg(long, short)]
+    input: Option<String>,
+    /// Space width for emoji matrix
+    #[arg(long, short)]
     space_width: usize,
+    /// Background character
+    #[arg(long, short)]
+    background_char: Option<char>,
+    /// Character spacing
+    #[arg(long, short)]
+    character_spacing: Option<usize>,
+    /// Print generated output
+    #[arg(long, action = ArgAction::SetTrue)]
+    print_output: bool,
 }
 
 fn main() {
     let args = Args::parse();
 
     let mut clipboard = Clipboard::new().unwrap();
+
     let mut input = String::new();
-    println!("Text to emojiify: ");
-    io::stdin().read_line(&mut input).unwrap();
+    if let Some(args_input) = args.input {
+        input = args_input;
+        println!("Text to emojify: {}", input.trim());
+    } else {
+        println!("Text to emojify:");
+        io::stdin().read_line(&mut input).unwrap();
+    }
     let input = input.trim();
 
+    println!("Pasting emoji from clipboard");
     let clipboard_text = clipboard.get_text().unwrap();
-    let emoji = clipboard_text.chars().next().unwrap().to_string();
-    let space = " ".repeat(args.space_width);
+
+    let emoji = clipboard_text.chars().next().unwrap_or('@').to_string();
+    let background = args
+        .background_char
+        .unwrap_or(' ')
+        .to_string()
+        .repeat(args.space_width);
 
     let mut matrices: Vec<Vec<Vec<String>>> = Vec::new();
 
@@ -33,7 +57,13 @@ fn main() {
                 .iter()
                 .map(|row| {
                     row.iter()
-                        .map(|&x| if x == 1 { emoji.clone() } else { space.clone() })
+                        .map(|&x| {
+                            if x == 1 {
+                                emoji.clone()
+                            } else {
+                                background.clone()
+                            }
+                        })
                         .collect()
                 })
                 .collect();
@@ -47,21 +77,28 @@ fn main() {
         return;
     }
 
-    let mut output_rows: Vec<String> = vec![];
+    let spacing = args
+        .background_char
+        .unwrap_or(' ')
+        .to_string()
+        .repeat(args.character_spacing.unwrap_or(args.space_width));
 
-    let num_rows = matrices[0].len();
-    for row_index in 0..num_rows {
-        let mut row_parts: Vec<String> = vec![];
-
-        for matrix in &matrices {
-            let row_str: String = matrix[row_index].concat();
-            row_parts.push(row_str);
-        }
-
-        let joined_row = row_parts.join("  ");
-        output_rows.push(joined_row);
-    }
+    let output_rows: Vec<String> = (0..matrices[0].len())
+        .map(|i| {
+            matrices
+                .iter()
+                .map(|m| m[i].concat())
+                .collect::<Vec<_>>()
+                .join(&spacing)
+        })
+        .collect();
 
     let result = output_rows.join("\n");
     clipboard.set_text(&result).unwrap();
+
+    println!("Copied generated emoji to clipboard");
+
+    if args.print_output {
+        println!("{}", result);
+    }
 }
